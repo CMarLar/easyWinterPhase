74c837403497719eac693cec112d10159a9fe407
CMarLar <c.marina.larrosa@gmail.com>
2022-12-13 15:37:47 +0100

    página gestión de casas completa

--- src/app/pages/houses-management/houses-management.component.ts
+++ src/app/pages/houses-management/houses-management.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AddPlayersComponent } from '../add-players/add-players.component';
 
 @Component({
   selector: 'app-houses-management',
@@ -15,14 +16,63 @@ export class HousesManagementComponent {
   public houseName:string;
   public asignedHouse:boolean;
 
+  public players;
+
   constructor(){
-    this.profilePic="../../../assets/img/carlos-marina_9131-bw.jpg"
+
     this.campaignName="Campaña de Carlos"
     this.shieldImage="../../../assets/img/escudo1.png"
     this.emptyShield="../../../assets/img/emptyshield.png"
-    this.playerName="Carlos M"
-    this.houseName="Salisbury"
+    this.profilePic="../../../assets/img/carlos-marina_9131-bw.jpg"
+
+    //Valida para cambiar el botón
     this.asignedHouse=true
+
+    this.players=[
+      
+      {id: 1, jugador:"Carlos M",casa:"Salisbury",escudo:this.shieldImage},
+      {id: 2, jugador:"Irene",casa:"Berwick",escudo:this.shieldImage},
+      {id: 3, jugador:"Carlos M",casa:"Salisbury",escudo:this.shieldImage},
+      // {id: 4, jugador:"Pepe",casa:null,escudo:this.emptyShield},
+      // {id: 5, jugador:"Miguel",casa:null,escudo:this.emptyShield},
+      {id: 6, jugador:"Miguel",casa:"López",escudo:this.shieldImage},
+    ]
+    this.checkPlayersReady()
+
   }
 
+  //Esta función cambia las casas a null, en ngIf del html hace que si es null, borre la pluma y la calavera.
+  public deleteHouse(number:number){
+    console.log(number);
+  for (let player of this.players){
+
+      if(player.id == number){
+      console.log(player.casa);
+      
+      player.casa = null
+      player.escudo = this.emptyShield
+
+      console.log(player.casa);
+      }
+      
+    }
+    this.checkPlayersReady()
+  }
+//COMPRUEBA que todos los jugadores están listos. No funciona el primero
+
+//arr.reduce((acumulador, valorActual[, índice[, array]]) =>[, valorInicial])
+
+  public checkPlayersReady():void{
+    this.asignedHouse = !this.players.reduce((acc,current)=>{
+      return acc && current.casa!=null},true)
+
+    console.log(this.asignedHouse);
+    
+
+  }
+
+
+
 }
+
+
