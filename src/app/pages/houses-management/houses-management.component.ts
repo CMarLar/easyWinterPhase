f8659eae892ed769c05d0db3a61dedde4b58862b
CMarLar <c.marina.larrosa@gmail.com>
2022-12-13 12:20:26 +0100

    página gestión de casas

--- src/app/pages/houses-management/houses-management.component.ts
+++ src/app/pages/houses-management/houses-management.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HousesManagementComponent {
 
+  public profilePic:string;
+  public campaignName:string;
+  public shieldImage:string;
+  public emptyShield:string;
+  public playerName:string;
+  public houseName:string;
+  public asignedHouse:boolean;
+
+  constructor(){
+    this.profilePic="../../../assets/img/carlos-marina_9131-bw.jpg"
+    this.campaignName="Campaña de Carlos"
+    this.shieldImage="../../../assets/img/escudo1.png"
+    this.emptyShield="../../../assets/img/emptyshield.png"
+    this.playerName="Carlos M"
+    this.houseName="Salisbury"
+    this.asignedHouse=true
+  }
+
 }
