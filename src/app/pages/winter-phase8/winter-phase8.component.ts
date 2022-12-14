ddee0ec8f65cc436e6f9e39649080a99f3f7087a
beckergirl <116119849+beckergirl@users.noreply.github.com>
2022-12-14 17:38:21 +0100

    phase 8

--- src/app/pages/winter-phase8/winter-phase8.component.ts
+++ src/app/pages/winter-phase8/winter-phase8.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class WinterPhase8Component {
 
-}
+
+  public miUsuario: Usuario;
+  WinterPhaseMainComponent: any;
+  jugadores: any;
+  
+   constructor(){
+    
+    this.miUsuario = new Usuario( 200, "Irene", "Herrero Becker", "irenisima82@hotmail.com","../../../assets/img/escudo10.png","Serpiente");
+
+   }
+  
+  }
+  
+  export class Usuario {
+  
+    public id_usuario: number
+    public nombre: string
+    public apellidos: string
+    public correo:string
+    public imagen_escudo: string
+    public password: string
+  
+  
+    constructor(id_usuario: number, nombre: string, apellidos: string, correo: string, imagen_escudo: string, password: string){
+  
+        this.id_usuario = id_usuario;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.imagen_escudo = imagen_escudo;
+        this.password = password
+  
+    }
+  }
