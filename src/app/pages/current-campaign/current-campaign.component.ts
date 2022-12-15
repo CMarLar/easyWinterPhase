e179b1c5bebe0fa243e069ecb2338f6122b69504
CMarLar <c.marina.larrosa@gmail.com>
2022-12-15 14:56:59 +0100

    addnpc sin algunas funcionalidades inplementadas

--- src/app/pages/current-campaign/current-campaign.component.ts
+++ src/app/pages/current-campaign/current-campaign.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CurrentCampaignComponent {
 
+  public campaignName;
+
+    constructor(){
+      this.campaignName="Campaña cojonuda"
+    }
+
 }
