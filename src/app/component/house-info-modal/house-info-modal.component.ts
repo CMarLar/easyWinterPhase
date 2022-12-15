b5ec7580247aa770ecb0cb7b7b4f11dcc8fa1bed
MiguelGeneroso <gene17051996@gmail.com>
2022-12-15 22:50:09 +0100

    creacion de modal infoCasas

--- src/app/component/house-info-modal/house-info-modal.component.ts
+++ src/app/component/house-info-modal/house-info-modal.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HouseInfoModalComponent {
 
+  public nombreCasa:string;
+  public feudo:string;
+  public caracteristicaFamiliar:string;
+  public nivelManutencion:string;
+  public nombrePJ:string;
+  public edad:string;
+  public nombreEscudero:string;
+  public house : any;
+  public npc : any[];
+  public nombreJugador : string;
+
+  constructor(){
+    this.npc = [{nombre : "Carlos",estado : true,rol : "esposa"},{nombre : "Irene",estado : false, rol : "hijo"},{nombre : "Miguel",estado : true, rol : "escudero"},{nombre : "Javier",estado : true, rol : "amante"},{nombre : "Carlos",estado : true,rol : "esposa"},{nombre : "Irene",estado : false, rol : "hijo"},{nombre : "Miguel",estado : true, rol : "escudero"},{nombre : "Javier",estado : true, rol : "amante"},{nombre : "Carlos",estado : true,rol : "esposa"},{nombre : "Irene",estado : false, rol : "hijo"},{nombre : "Miguel",estado : true, rol : "escudero"},{nombre : "Javier",estado : true, rol : "amante"}];
+    this.nombrePJ = "Pepe";
+    this.nombreJugador = "Miguel";
+    this.feudo = "../../../assets/img/escudo1.png";
+    this.nombreCasa = "Casa Newton";
+    this.caracteristicaFamiliar = "Buenos con los caballos"
+    this.house = {nombreJugador : this.nombreJugador,
+      nombreCasa:this.nombreCasa,
+      feudo:this.feudo,
+      caracteristicaFamiliar:this.caracteristicaFamiliar,
+      nivelManutencion:this.nivelManutencion,
+      nombrePJ:this.nombrePJ,
+      edad:this.edad,
+      nombreEscudero:this.nombreEscudero,
+      npc:this.npc}
+  }
 }
