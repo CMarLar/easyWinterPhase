94f9e832bca3922dc059813db2807942a6017f22
MiguelGeneroso <gene17051996@gmail.com>
2022-12-16 11:18:10 +0100

    modal aggin empezado

--- src/app/component/aging-modal/aging-modal.component.ts
+++ src/app/component/aging-modal/aging-modal.component.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./aging-modal.component.css']
 })
 export class AgingModalComponent {
+  
+  public pj : any = {};
+  public pnj : any = [];
+  public tirada : number[];
 
+  constructor(){
+    this.tirada = [1,1,1,7];
+    this.pj = {
+      nombre : "Manolo",
+      edad : 36,
+      escudero : {
+                  nombre : "Alfred",
+                  edad : 21,
+      },
+      estats : {tamaño : 10,
+                destreza : 11,
+                fuerza : 12,
+                constitucion : 13,
+                apariencia : 14
+              }
+    }
+    this.pnj = [{nombre : "Antonio",
+    edad : 40,
+    estats : {tamaño : 10,
+              destreza : 11,
+              fuerza : 12,
+              constitucion : 13,
+              apariencia : 14
+            }},
+          {nombre : "Pedro",
+    edad : 33,
+    estats : {tamaño : 10,
+              destreza : 11,
+              fuerza : 12,
+              constitucion : 13,
+              apariencia : 14
+            }},
+          {nombre : "Maria",
+    edad : 27,
+    estats : {tamaño : 10,
+              destreza : 11,
+              fuerza : 12,
+              constitucion : 13,
+              apariencia : 14
+            }},
+          {nombre : "Felipe",
+    edad : 24,
+    estats : {tamaño : 10,
+              destreza : 11,
+              fuerza : 12,
+              constitucion : 13,
+              apariencia : 14
+            }},
+          {nombre : "Josefa",
+    edad : 30,
+    estats : {tamaño : 10,
+              destreza : 11,
+              fuerza : 12,
+              constitucion : 13,
+              apariencia : 14
+            }}]
+  }
 }
