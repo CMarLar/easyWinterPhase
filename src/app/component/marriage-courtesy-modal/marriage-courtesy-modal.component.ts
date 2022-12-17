708d07b537cfdf75b80fa4a3ada8e1d0e6afb622
MiguelGeneroso <gene17051996@gmail.com>
2022-12-17 19:56:42 +0100

    creacion de modales matrimonio

--- src/app/component/marriage-courtesy-modal/marriage-courtesy-modal.component.ts
+++ src/app/component/marriage-courtesy-modal/marriage-courtesy-modal.component.ts
@@ -7,4 +7,122 @@ import { Component } from '@angular/core';
 })
 export class MarriageCourtesyModalComponent {
 
+  public resultadoMatrimonio : boolean;
+  public isHidden : boolean;
+  public rol : string;
+  public resultadoTirada : number;
+  public resultadoFinal : string;
+  public gloria : number;
+  public libras : string;
+  public casarse : boolean;
+  public casadoCon : boolean;
+  public noRelleno :  boolean;
+  public objeto : any;
+  public botonPulsado : boolean;
+  public noSeCasa: string;
+
+  constructor(){
+    this.isHidden = true;
+    this.casarse = null;
+    this.casadoCon = false;
+    this.noRelleno = true;
+    this.botonPulsado = false;
+    this.noSeCasa = ""
+    this.objeto = {
+      rol : this.rol,
+      gloria : this.gloria,
+      libras : this.libras
+    }
+
+  }
+
+  //A PARTIR DE AQUI ES EL CASAMIENTO POR CORTESIA
+  //EN EL INPUT SE INTRODUCE EL VALOR DE CORTESIA
+  //SE TIRA UN DADO RANDOM QUE SI IGUALA O SUPERA EL VALOR DE CORTESIA SALDRA EXITO
+  //SI SALE EXITO SE INTRODUCIRA EL VALOR DE UNA TIRADA DE MATRIMONIO
+
+  public calcularMatrimonioCortesia(value : string){
+
+    let dado : number = Math.floor((Math.random() * 20) + 1);
+    if(dado >= parseInt(value)){
+      this.resultadoFinal = "Exito";
+      console.log(this.resultadoFinal);
+      
+    }else if (dado <= parseInt(value)){
+      this.resultadoFinal = "Fallo";
+    }
+
+  }
+
+  public casarseResult(){
+    this.casarse = true;
+    this.botonPulsado = true;
+  }
+
+  public esposaResult(value : string){
+
+    let dadoHija : number;
+    let dadoHermanos : number;
+
+      if(parseInt(value) <= 5){
+
+      this.rol = "Plebeya adinerada";
+      this.gloria = 0;
+      this.libras = "3d6 + 6£";
+      
+    }else if (parseInt(value) >= 6 && parseInt(value) <= 8){
+
+      dadoHija = Math.floor((Math.random() * 6) + 1);
+      this.rol = "Hija " + dadoHija + " de un escudero";
+      this.gloria = 10;
+      this.libras = "3£";
+
+    }else if (parseInt(value) >= 9 && parseInt(value) <= 10){
+
+      dadoHija = Math.floor((Math.random() * 6) + 1);
+      this.rol = "Hija " + dadoHija + "de caballero mantenido";
+      this.gloria = 50;
+      this.libras = "1d6 £";
+
+    }else if (parseInt(value) == 11){
+
+      this.rol = "Hija mayor de caballero vasallo adinerado";
+      this.gloria = 100;
+      this.libras = "1d3 + 6£";
+
+    }else if (parseInt(value) >= 12 && parseInt(value) <= 20){
+
+      dadoHija = Math.floor((Math.random() * 6) + 1);
+      dadoHermanos = Math.floor((Math.random() * 6) + 1);
+      this.rol = "Hija " + dadoHija + "/" + dadoHermanos + " de caballero vasallo";
+      this.gloria = 100;
+      this.libras = "1d6 £";
+
+    }else if (parseInt(value) >= 21 && parseInt(value) <= 25){
+
+      this.rol = "Heredera de caballero vasallo";
+      this.gloria = 100;
+      this.libras = "1 señorío, 1d6+ 10£";
+
+    }
+    else if (parseInt(value) >= 26 && parseInt(value) <= 27){
+
+      this.rol = "Heredera de caballero vasallo adinerado";
+      this.gloria = 300;
+      this.libras = "2 señoríos, 1d6 £";
+
+    }else if (parseInt(value) >= 28){
+
+      this.rol = "Hija más joven de un barón";
+      this.gloria = 250;
+      this.libras = "1 señorío, 1d6+ 10£"
+
+    }
+    this.casadoCon = true;  
+  }
+
+  public noMarriage(){
+    this.noSeCasa = "No";
+    this.botonPulsado = true;
+  }
 }
--- src/app/component/marriage-loyalty-modal/marriage-loyalty-modal.component.ts
+++ src/app/component/marriage-loyalty-modal/marriage-loyalty-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-marriage-loyalty-modal',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class MarriageLoyaltyModalComponent {
 
+  public resultadoMatrimonio : boolean;
+  public isHidden : boolean;
+  public rol : string;
+  public resultadoTirada : number;
+  public resultadoFinal : string;
+  public gloria : number;
+  public libras : string;
+
+  constructor(){
+    this.isHidden = true;
+  }
+
+  //EN EL INPUT SE INTRODUCE EL VALOR POR LEALTAD EJ: 16
+  //SE TIRARA UN DADO RANDOM QUE SI IGUALA O SUPERA EL VALOR DE LEALTAD SALDRA EXITO
+  //SI SALE EXITO SE SUMA 1d6 A LA DOTE Y 10 A LA GLORIA
+  //SI SALE FALLO EL SEÑOR NEGARA EL CASAMIENTO
+
+  public calcularMatrimonioLealtad(lealtad : string){
+    // this.isHidden = true;
+    let dado : number = Math.floor((Math.random() * 20) + 1);
+
+    if (dado >= parseInt(lealtad)){
+      this.resultadoTirada = dado;
+      this.resultadoFinal = "Exito";
+      this.libras = "1d6 £";
+      this.gloria = 10
+    }else{
+      
+      this.resultadoFinal = "Fallo";
+      this.libras = null;
+      this.gloria = null;
+      console.log(this.resultadoFinal);
+    }
+
+    this.isHidden = true;
+  }
+  
 }
