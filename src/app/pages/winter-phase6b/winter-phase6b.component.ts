21d5331839f01bbec20a7b438a24d62f513a68f7
beckergirl <116119849+beckergirl@users.noreply.github.com>
2022-12-17 12:46:52 +0100

    phase6b

--- src/app/pages/winter-phase6b/winter-phase6b.component.ts
+++ src/app/pages/winter-phase6b/winter-phase6b.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class WinterPhase6bComponent {
 
+  public nombre: string
+  public apellidos: string
+  public foto_escudo : string
+  public cisrcunstanciaEconomica: number
+  public edadMadre: number
+  public nombreAmante: string
+  public nombreAmante2: string
+  public nombreEsposa: string
+  public rol_esposa: string
+  public rol_amante: string
+
+  constructor(){
+
+    this.nombre = "Irene"
+    this.apellidos = "Herrero Becker"
+    this.foto_escudo = "../../../assets/img/escudo10.png"
+    this. cisrcunstanciaEconomica = -2
+    this.edadMadre = -1
+    this.nombreEsposa = "Theresa"
+    this.nombreAmante = "Abigail"
+    this.nombreAmante2 = "Lynn"
+    this.rol_amante = "Amante"
+    this.rol_esposa = "Esposa"
+   
+
+   }
 }
+
+function calcularHijos(){
+
+  return ;
+}
+
+function matrimonioCortesia(modal2){
+
+  return modal2
+}
+
