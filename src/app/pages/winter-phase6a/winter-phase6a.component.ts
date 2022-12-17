fa91872eee0e9368af9a99f9c763d34163dd99b6
beckergirl <116119849+beckergirl@users.noreply.github.com>
2022-12-17 10:28:50 +0100

    phase6a

--- src/app/pages/winter-phase6a/winter-phase6a.component.ts
+++ src/app/pages/winter-phase6a/winter-phase6a.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { ModifierFlags } from 'typescript';
 
 @Component({
   selector: 'app-winter-phase6a',
   templateUrl: './winter-phase6a.component.html',
   styleUrls: ['./winter-phase6a.component.css']
 })
+
 export class WinterPhase6aComponent {
 
+  public nombre: string
+  public apellidos: string
+  public foto_escudo : string
+
+  constructor(){
+
+    this.nombre = "Irene",
+    this.apellidos = "Herrero Becker"
+    this.foto_escudo = "../../../assets/img/escudo10.png"
+
+   }
+}
+
+function matrimonioLealtad(modal1){
+
+  return modal1;
+}
+
+function matrimonioCortesia(modal2){
+
+  return modal2
 }
