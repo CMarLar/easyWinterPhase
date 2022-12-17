a4844a4cdac8d31ccd68fccab8b3d993d9f2718f
MiguelGeneroso <gene17051996@gmail.com>
2022-12-17 20:41:38 +0100

    creacion de modal births

--- src/app/component/births-modal/births-modal.component.ts
+++ src/app/component/births-modal/births-modal.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class BirthsModalComponent {
 
+  public modEconomia : number;
+  public modEdadMadre : number;
+  public nombreHijo : string;
+  public madre : any;
+  public sexo : string;
+
+  constructor(){
+    this.madre = {nombre : "Ana",
+                  edad : 36,
+                  partoUltimoAño : true};
+    this.sexo = "niño";
+    this.modEconomia = -2;
+    this.modEdadMadre = -1;
+  }
 }
