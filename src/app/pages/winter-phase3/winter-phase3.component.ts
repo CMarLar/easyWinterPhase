0ceb184a1772638402160756f9758543fea29cd0
MiguelGeneroso <gene17051996@gmail.com>
2022-12-18 15:13:25 +0100

    phase3 modificada

--- src/app/pages/winter-phase3/winter-phase3.component.ts
+++ src/app/pages/winter-phase3/winter-phase3.component.ts
@@ -20,6 +20,12 @@ export class WinterPhase3Component {
   public edad_personaje2: number
   public nombre_pnj: string
 
+
+  public pj : any;
+  public pnj : any;
+  public isHide : boolean;
+  public newEscudero : any;
+
    constructor(){
     
     this.nombre = "Irene",
@@ -31,14 +37,57 @@ export class WinterPhase3Component {
     this.personaje2 = "Belengarius"
     this.nombre_pnj = "Julian"
 
+
+    this.pj = {nombre : "Antonito",
+              edad : 33,
+              escudero : {nombre : "pedro",
+                          edad : 19}
+            }
+    this.pnj = [{nombre : "Belengarius", edad : 26},
+                {nombre : "Espartacus" , edad :35},
+                {nombre : "Alfrodo" , edad :36},
+                {nombre : "Morfinus" , edad : 32},
+                {nombre : "Anulus", edad : 40}];
+
+    this.isHide = true;
+
+
    }
+
+   public envejecimientoAutomatico(){
+
+    
+    if(this.isHide == true){
+        this.isHide = false;
+    }else{
+      this.isHide = true;
+    }
+
+    
+   
+  }
+
+  nuevoEscudero(nombreEscudero : string){
+    this.newEscudero = {nombre : nombreEscudero,
+                        edad : 15}
+  }
+
+  public sumarAge(){
+
+    this.pj.edad = this.pj.edad + 1;
+    this.pj.escudero.edad = this.pj.escudero.edad + 1;
+
+    for(let i = 0; i < this.pnj.length; i++){
+
+      this.pnj[i].edad = this.pnj[i].edad + 1;
+    }
+
+    if (this.pj.escudero.edad >= 21){
+      this.pj.escudero = this.newEscudero;
+    }
+
+  }
   
   }
-/* envejecimiento otros personaje (Belengarius 38) */
-function envejecimientoAuto(edad_personaje: number){
-
-  return (edad_personaje + 1);
-}
-
 
 
