c94f1ae034a3b719de312ca763cc0f70a8cef319
MiguelGeneroso <gene17051996@gmail.com>
2022-12-21 14:17:49 +0100

    servicio y funcionalidad login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-login',
@@ -13,35 +15,63 @@ export class LoginComponent {
   public usuarioConectado : boolean;
   public dataOk : boolean;
 
-  constructor(private router : Router){
+  constructor(private router : Router, private userService : UserService){
     this.user = "miguelGeneroso@gmail.com";
     this.password = "contraseñaMiguel";
     this.usuarioConectado = false;
     this.dataOk = true;
   }
 
-  public userValidate(user : string,password : string){
+  public userValidate(email : string,password : string){
 
-    console.log(this.user);
-    console.log(this.password);
+    // console.log(this.user);
+    // console.log(this.password);
+    // console.log(user);
+    // console.log(password);
     
+    this.userService.login(new User(password,email,null,null,null))
+    .subscribe((data : User[]) => {
+
+      console.log(data);
+      if(data[0] != undefined){
+
+        this.userService.user = new User(data[0].password,data[0].email,data[0].avatar,data[0].username,data[0].id_user);
+        this.userService.logueado = true;
+        console.log("LOGUEADO");
+        this.goPlace();
+        
+
+      }else{
+
+        this.userService.logueado = false;
+        this.dataOk = false;
+        
+      }
+      
+    })
     
-    console.log(user);
-    console.log(password);
-    
-    
-    if (user == this.user && password == this.password){
 
-      this.router.navigateByUrl('/campaigns');
-      this.usuarioConectado = true;
 
-    }else{
 
-      console.log("Error al introducir el usuario o la contraseña");
-      this.usuarioConectado = false;
-      this.dataOk = false;
 
-    }
+    // if (email == this.user && password == this.password){
+
+    //   this.router.navigateByUrl('/campaigns');
+    //   this.usuarioConectado = true;
+
+    // }else{
+
+    //   console.log("Error al introducir el usuario o la contraseña");
+    //   this.usuarioConectado = false;
+    //   this.dataOk = false;
+
+    // }
+
+  }
+
+  public goPlace(){
+
+    this.router.navigateByUrl('/campaigns');
 
   }
 }
